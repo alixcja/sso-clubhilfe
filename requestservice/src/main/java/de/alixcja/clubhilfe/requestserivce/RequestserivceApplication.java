98c702ed754959de360b970c4db24588bcf37cfb
alixcja <alicja.pater.2004@gmail.com>
2025-04-27 22:43:21 +0200

    🎉 Start requestservice

---
+++ requestservice/src/main/java/de/alixcja/clubhilfe/requestserivce/RequestserivceApplication.java
@@ -0,0 +1,13 @@
+package de.alixcja.clubhilfe.requestserivce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestserivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestserivceApplication.class, args);
+	}
+
+}
---
+++ requestservice/src/test/java/de/alixcja/clubhilfe/requestserivce/RequestserivceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.alixcja.clubhilfe.requestserivce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestserivceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

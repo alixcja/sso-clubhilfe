085638682809a2bbc93665e8901ca8a5879a951b
alixcja <alicja.pater.2004@gmail.com>
2025-05-27 13:40:24 +0200

    🚚 Rename RequestserivceApplication to RequestServiceApplication

--- requestservice/src/main/java/de/alixcja/clubhilfe/requestserivce/RequestServiceApplication.java
+++ requestservice/src/main/java/de/alixcja/clubhilfe/requestserivce/RequestServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RequestserivceApplication {
+public class RequestServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RequestserivceApplication.class, args);
+		SpringApplication.run(RequestServiceApplication.class, args);
 	}
 
 }

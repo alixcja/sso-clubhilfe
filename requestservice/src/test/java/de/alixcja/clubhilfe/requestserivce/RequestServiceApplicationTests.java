3e7d7bd196017c915512ceee560ca3628e1ae5af
alixcja <alicja.pater.2004@gmail.com>
2025-05-27 22:33:23 +0200

    ✅ Fix RequestServiceApplicationTests

--- requestservice/src/test/java/de/alixcja/clubhilfe/requestserivce/RequestServiceApplicationTests.java
+++ requestservice/src/test/java/de/alixcja/clubhilfe/requestserivce/RequestServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RequestServiceApplicationTests {
+class RequestServiceApplicationTests extends PostgreSQLTestDatabase{
 
 	@Test
 	void contextLoads() {
